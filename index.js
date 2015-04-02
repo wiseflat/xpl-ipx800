92621f2b67403a9b27de584491d163e3724f71d2
Mathieu Garcia <mgarcia@wiseflat.com>
2015-04-02 16:47:54 +0200

    evo

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var xplipx800 = require("./lib/xpl-ipx800");
+var schema_ipx800basic = require('/etc/wiseflat/schemas/ipx800.basic.json');
+var schema_ipx800config = require('/etc/wiseflat/schemas/ipx800.config.json');
 
 var wt = new xplipx800(null, {
 	//xplSource: 'bnz-ipx800.wiseflat'
@@ -11,7 +13,13 @@ wt._init(function(error, xpl) {
 		return;
 	}
         
-        wt.getLocalConfig();
+	xpl.addBodySchema(schema_ipx800basic.id, schema_ipx800basic.definitions.body);
+	xpl.addBodySchema(schema_ipx800config.id, schema_ipx800config.definitions.body);
+
+	// Load config file into hash
+        wt.readConfig();
+	
+        //wt.getLocalConfig();
         
         xpl.on("xpl:ipx800.basic", function(evt) {
 		console.log("Receive message ipx800.basic ", evt);
